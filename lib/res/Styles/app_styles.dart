53a5ba78cfed020a1df498505eb0e10125067e5f
Gontse Ranoto (Gontze) <gontseandries@gmail.com>
2024-07-15 18:17:11 +0200

    Updated App Styles

--- lib/res/Styles/app_styles.dart
+++ lib/res/Styles/app_styles.dart
@@ -6,10 +6,12 @@ Color primary = const Color(0xFF687daf);
 class AppStyles{
  static Color primaryColor = primary;
  static Color textColor = const Color(0xFF3b3b3b);
+ static Color bgColor = const Color(0xFFeeedf2);
+
 
  static TextStyle headlineStyle1 = TextStyle(
      fontSize: 26,
-     fontWeight: FontWeight.w500,
+     fontWeight: FontWeight.bold,
      color: textColor);
 
  static TextStyle headlineStyle3 = const TextStyle(
