2b000152f0a7867bf093a77ae621c801314ad4b8
Gontse Ranoto (Gontze) <gontseandries@gmail.com>
2024-07-16 05:58:29 +0200

    Added Big Dot Widget

--- lib/Core/Widgets/ticket_view.dart
+++ lib/Core/Widgets/ticket_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_app/Core/Widgets/big_dot.dart';
+import 'package:ticket_app/res/Styles/app_styles.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({super.key});
@@ -13,7 +15,33 @@ class TicketView extends StatelessWidget {
       height: 178,
       child: Container(
         margin: EdgeInsets.only(right: 16),
-        color: Colors.red,
+        padding: EdgeInsets.all(16),
+        child: Center(child: Column(
+          children: [
+            Row(
+              children: [
+                Text("NYC", style: AppStyles.headlineStyle3.copyWith(
+                  color: Colors.white),),
+                Expanded(child: Container()),
+                BigDot(),
+                Expanded(child: Stack(children: [
+                  SizedBox(
+                    child: Text("----------------"),
+                  ),
+                  Center(child: Text("Plane"),)
+                ],)),
+                BigDot(),
+                Expanded(child: Container()),
+                Text("NYC", style: AppStyles.headlineStyle3.copyWith(
+                    color: Colors.white),),
+              ],
+            )
+          ],
+        )),
+        decoration: BoxDecoration(
+          color: AppStyles.ticketBlue,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(21), topRight: Radius.circular(21))),
       ),
     );
   }
