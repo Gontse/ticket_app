f28493d549f8b4196c989b1614ca8041ad90e106
Gontse Ranoto (Gontze) <gontseandries@gmail.com>
2024-07-17 03:05:48 +0200

    Completed Plane ticket pt1

--- lib/Core/Widgets/ticket_view.dart
+++ lib/Core/Widgets/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_app/Core/Widgets/app_layoutbuilder.dart';
 import 'package:ticket_app/Core/Widgets/big_dot.dart';
 import 'package:ticket_app/res/Styles/app_styles.dart';
 
@@ -8,40 +9,55 @@ class TicketView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
     return SizedBox(
-      width: size.width*0.85,
+      width: size.width * 0.85,
       height: 178,
       child: Container(
-        margin: EdgeInsets.only(right: 16),
-        padding: EdgeInsets.all(16),
-        child: Center(child: Column(
+        margin: const EdgeInsets.only(right: 16),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            color: AppStyles.ticketBlue,
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(21), topRight: Radius.circular(21))),
+        child: Center(
+            child: Column(
           children: [
             Row(
               children: [
-                Text("NYC", style: AppStyles.headlineStyle3.copyWith(
-                  color: Colors.white),),
+                Text(
+                  "NYC",
+                  style: AppStyles.headlineStyle3.copyWith(color: Colors.white),
+                ),
                 Expanded(child: Container()),
-                BigDot(),
-                Expanded(child: Stack(children: [
-                  SizedBox(
-                    child: Text("----------------"),
-                  ),
-                  Center(child: Text("Plane"),)
-                ],)),
-                BigDot(),
+                const BigDot(),
+                Expanded(
+                    child: Stack(children: [
+                  const SizedBox(
+                      height: 24,
+                      child: AppLayoutBuilderWidget(
+                        randomDivider: 5,
+                      )),
+                  Center(
+                    child: Transform.rotate(
+                      angle: 1.5,
+                      child: const Icon(
+                        Icons.local_airport_rounded,
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ])),
+                const BigDot(),
                 Expanded(child: Container()),
-                Text("NYC", style: AppStyles.headlineStyle3.copyWith(
-                    color: Colors.white),),
+                Text(
+                  "RSA",
+                  style: AppStyles.headlineStyle3.copyWith(color: Colors.white),
+                ),
               ],
             )
           ],
         )),
-        decoration: BoxDecoration(
-          color: AppStyles.ticketBlue,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(21), topRight: Radius.circular(21))),
       ),
     );
   }
