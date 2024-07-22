4f2a41a314b3517cb01e6da9363e0bff07a1a8e5
Gontse Ranoto (Gontze) <gontseandries@gmail.com>
2024-07-22 16:40:16 +0200

    Update app_double_text.dart

--- lib/Core/Widgets/app_double_text.dart
+++ lib/Core/Widgets/app_double_text.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:ticket_app/res/Styles/app_styles.dart';
+import 'package:ticket_app/screens/all_tickets.dart';
 
 class AppDoubleText extends StatelessWidget {
   const AppDoubleText({super.key, required this.bigText, required this.smallText });
@@ -19,7 +20,10 @@ class AppDoubleText extends StatelessWidget {
             color: AppStyles.primaryColor
           )) ,
           onTap: () {
-
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (BuildContext context) => const AllTickets() )
+            );
           },
         )
       ],
