dd1b546a4b3aaed1b479152b480c244210a462ba
Gontse Ranoto (Gontze) <gontseandries@gmail.com>
2024-07-22 17:34:07 +0200

    Update all_tickets.dart

--- lib/screens/all_tickets.dart
+++ lib/screens/all_tickets.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/Core/Widgets/Utils/TestData/all_json.dart';
+import 'package:ticket_app/Core/Widgets/ticket_view.dart';
 
 class AllTickets extends StatelessWidget {
   const AllTickets({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("All Tickets"),
+      ),
+      body: ListView(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+                children: ticketList
+                    .map((ticket) => TicketView(ticket: ticket))
+                    .toList()),
+          ),
+        ],
+      ),
+    );
   }
 }
