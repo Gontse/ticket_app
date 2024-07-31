45c294791978ae52a944d79ab711c5ebd6997d74
Gontse Ranoto (Gontze) <gontseandries@gmail.com>
2024-07-31 08:34:14 +0200

    AppTicketTabs

--- lib/Search/search_screen.dart
+++ lib/Search/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_app/res/Styles/app_styles.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -6,8 +7,16 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppStyles.bgColor,
       body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      children: [
+        SizedBox(height: 40,),
+        Text("What are\nyou looking for?", style: AppStyles.headlineStyle1.copyWith(
+          fontSize: 35,
 
+        ),)
+      ],
       ),
     );
   }
