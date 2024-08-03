11c6548f86e15dc1c7e8e782cf11875fccb68be2
Gontse Ranoto (Gontze) <gontseandries@gmail.com>
2024-08-03 05:00:36 +0200

    Update search_screen.dart

--- lib/Search/search_screen.dart
+++ lib/Search/search_screen.dart
@@ -94,32 +94,47 @@ class SearchScreen extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                    width: size.width * .44,
-                    height: 210,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: const Color(0xFF3AB8B8)
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Discount\nfor survey",
-                          style: AppStyles.headlineStyle2.copyWith(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white
-                          ),),
-                        const SizedBox(height: 10),
-                        Text("Take the survey about services and get discount",
-                          style: AppStyles.headlineStyle2.copyWith(
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white,
-                            fontSize: 18
+                  Stack(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                        width: size.width * .44,
+                        height: 210,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color(0xFF3AB8B8)
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Discount\nfor survey",
+                              style: AppStyles.headlineStyle2.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white
+                              ),),
+                            const SizedBox(height: 10),
+                            Text("Take the survey about services and get discount",
+                              style: AppStyles.headlineStyle2.copyWith(
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                  fontSize: 18
 
-                          ),),
-                      ],
-                    ),
+                              ),),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(30),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 18,
+                              color: const Color(0xFF1899999),
+
+                            )
+                        ),
+                      )
+                    ],
                   )
                 ],
               )
